2f5242da34c7e334453b52b56297e9812dc674bf
pmanonline <petersonzoconis@gmail.com>
2024-04-01 12:24:02 +0100

    updated the url

--- index.js
+++ index.js
@@ -1,57 +1,3 @@
-// const express = require("express");
-// const morgan = require("morgan");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// const app = express();
-// const bodyParser = require("body-parser");
-// const ClassUser = require("./models/classModel");
-
-// app.use(bodyParser.json());
-
-// // cors
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   })
-// );
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
-// app.options("*", function (req, res) {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your client's domain
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.sendStatus(200);
-// });
-// // * BorderParser Middleware
-// app.use(express.json());
-
-// // * Load Env
-// dotenv.config({ path: "./config.env" });
-
-// //* Log route actions
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan("dev"));
-// }
-// const options = {
-//   allowedHosts: ["localhost"], // Example of allowed hosts
-//   // Other options...
-// };
-// //* Use Routes
-// app.use("/api/course", require("./routes/course"));
-// app.use("/api/class", require("./routes/class"));
-// app.use("/api/student", require("./routes/student"));
-
-// const port = process.env.PORT || 5000;
-
-// app.listen(port, () => console.log(`Server started on port ${port}`));
-
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
@@ -65,7 +11,7 @@ app.use(bodyParser.json());
 // CORS middleware
 app.use(
   cors({
-    origin: "https://schoolmgt-app.vercel.app",
+    origin: ["https://schoolmgt-app.vercel.app/", "http://localhost:10000"],
     credentials: true,
   })
 );

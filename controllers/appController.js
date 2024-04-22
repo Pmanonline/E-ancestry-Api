1846b2e4cf05147ebbc676b58d5f5568f52d434f
pmanonline <petersonzoconis@gmail.com>
2024-04-22 17:48:11 +0100

    updating mailer

--- controllers/appController.js
+++ controllers/appController.js
@@ -217,8 +217,9 @@ const generateOTP = asyncHandler(async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        // user: process.env.EMAIL_USER,
+        user: "petersonzoconis@gmail.com",
+        pass: "hszatxfpiebzavdd",
       },
     });
 
